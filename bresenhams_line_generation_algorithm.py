dcdc08189950c0d622ccd037bca1946a0049fbfb
danoneyal <97377300+danoneyal@users.noreply.github.com>
2022-01-12 09:39:07 +0200

    adding unit test ny using line array

--- bresenhams_line_generation_algorithm.py
+++ bresenhams_line_generation_algorithm.py
@@ -25,7 +25,12 @@ pixel_position_array = []
 class BresenhamsLineGenerationAlgorithm(object):
 
     @staticmethod
-    def bresenham(x1, y1, x2, y2, step_size=1):
+    def bresenham(start_position: PixelPosition, end_position: PixelPosition, step_size=1):
+        x1 = start_position.horizontal
+        y1 = start_position.vertical
+        x2 = end_position.horizontal
+        y2 = end_position.vertical
+
         positive = 0
         negative = 1
         dirs_x = [step_size, -step_size]
@@ -54,7 +59,10 @@ class BresenhamsLineGenerationAlgorithm(object):
                 calc_result_array[1, calc_result_array_index] = cur_position.vertical
                 calc_result_array[0, calc_result_array_index] = cur_position.horizontal
                 calc_result_array_index += 1
-
+                pixel_position_array.append(copy.deepcopy(cur_position))
+                print("(", x1, ",", y, ")\n")
+                # end of test
+            return
         # check the slope of the line ,delta_y_smaller_then_delta_x -1<m<1,abs(m)<1
         if math.fabs(y2 - y1) < math.fabs(x2 - x1):
             m_new = 2 * math.fabs(y2 - y1)
@@ -70,7 +78,9 @@ class BresenhamsLineGenerationAlgorithm(object):
                 calc_result_array[1, calc_result_array_index] = cur_position.vertical
                 calc_result_array[0, calc_result_array_index] = cur_position.horizontal
                 calc_result_array_index += 1
+                pixel_position_array.append(copy.deepcopy(cur_position))
                 print("(", x, ",", y, ")\n")
+                # end of test
 
                 # Add slope to increment angle formed
                 slope_error_new = slope_error_new + m_new
@@ -81,7 +91,6 @@ class BresenhamsLineGenerationAlgorithm(object):
                     y = y + step_size_y
                     slope_error_new = slope_error_new - 2 * math.fabs(x2 - x1)
 
-                pixel_position_array.append(copy.deepcopy(cur_position))
         else:  # delta_y_greater_then_delta_x -1<m<-1, abs(m)>1
             m_new = math.fabs(2 * (x2 - x1))
             slope_error_new = m_new - math.fabs(x2 - x1)
@@ -96,8 +105,9 @@ class BresenhamsLineGenerationAlgorithm(object):
                 calc_result_array[1, calc_result_array_index] = cur_position.vertical
                 calc_result_array[0, calc_result_array_index] = cur_position.horizontal
                 calc_result_array_index += 1
-
+                pixel_position_array.append(copy.deepcopy(cur_position))
                 print("(", x, ",", y, ")\n")
+                #end of test
 
                 # Add slope to increment angle formed
                 slope_error_new = slope_error_new + m_new
@@ -107,7 +117,5 @@ class BresenhamsLineGenerationAlgorithm(object):
                 if slope_error_new >= 0:
                     x = x + step_size_x
                     slope_error_new = slope_error_new - math.fabs(2 * (y2 - y1))
-
-                pixel_position_array.append(copy.deepcopy(cur_position))
 
         use_for_break_point = 0
--- using_bresenhams_line_generation_algorithm.py
+++ using_bresenhams_line_generation_algorithm.py
@@ -1,65 +1,117 @@
-from bresenhams_line_generation_algorithm import *
-import bresenhams_line_generation_algorithm as bla
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
-    # m slope is infinite
-#delta_x_zero__positive_y_direction(self):
+import bresenhams_line_generation_algorithm as bla
+from bresenhams_line_generation_algorithm import *
+
+
+class LineCoordinate:
+    def __init__(self):
+        self.start_position = PixelPosition()
+        self.end_position = PixelPosition()
+
+line = LineCoordinate()
+line_array = []
+
+# line.start_position = PixelPosition()
+# line.end_position = PixelPosition()
+
+
+# m slope is infinite
+# delta_x_zero__positive_y_direction(self):
 x1 = 45
 x2 = 45
 y1 = 1
 y2 = 45
-#delta_x_zero__negative_y_direction
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
+
+# delta_x_zero__negative_y_direction
 x1 = 45
 x2 = 45
 y1 = 45
 y2 = 1
 
-    # m slope is zero
-#delta_y_zero__positive_x_direction(self):
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
+
+# m slope is zero
+# delta_y_zero__positive_x_direction(self):
 
 x1 = 1
 x2 = 45
 y1 = 20
 y2 = 20
 
-#delta_y_zero__negative_x_direction(self):  #
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
+
+# delta_y_zero__negative_x_direction(self):  #
 
 x1 = 45
 x2 = 1
 y1 = 20
 y2 = 20
 
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
 
 # delta_y_smaller_then_delta_x -1<m<1,abs(m)<1
-#delta_y_smaller_then_delta_x__positive_x_positive_y_direction(self):  # -1<m<1, abs(m)<1, x1<x2 , y1<y2
+# delta_y_smaller_then_delta_x__positive_x_positive_y_direction(self):  # -1<m<1, abs(m)<1, x1<x2 , y1<y2
 
 x1 = 1
 x2 = 45
 y1 = 1
 y2 = 20
-
-#delta_y_smaller_then_delta_x__positive_x_negative_y_direction(self):  # -1<m<1, abs(m)<1,x1<x2 , y1>y2
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
+# delta_y_smaller_then_delta_x__positive_x_negative_y_direction(self):  # -1<m<1, abs(m)<1,x1<x2 , y1>y2
 
 x1 = 1
 x2 = 45
 y1 = 45
 y2 = 20
-
-#delta_y_smaller_then_delta_x__negative_x_positive_y_direction(self):  # -1<m<1, abs(m)<1,x1>x2 , y1<y2
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
+# delta_y_smaller_then_delta_x__negative_x_positive_y_direction(self):  # -1<m<1, abs(m)<1,x1>x2 , y1<y2
 
 x1 = 45
 x2 = 1
 y1 = 20
 y2 = 45
-
-#delta_y_smaller_then_delta_x__negative_x_negative_y_direction(self):  # -1<m<1, abs(m)<1, x1>x2 , y1>y2
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
+# delta_y_smaller_then_delta_x__negative_x_negative_y_direction(self):  # -1<m<1, abs(m)<1, x1>x2 , y1>y2
 x1 = 45
 x2 = 1
 y1 = 45
 y2 = 20
-
-
+line.start_position.horizontal = x1
+line.start_position.vertical = y1
+line.end_position.horizontal = x2
+line.end_position.vertical = y2
+line_array.append(copy.deepcopy(line))
 """
        
     # delta_y_greater_then_delta_x -1<m<-1, abs(m)>1
@@ -76,55 +128,55 @@ y2 = 20
         pass
     
 """
+for line in line_array:
+    bla.BresenhamsLineGenerationAlgorithm.bresenham(line.start_position, line.end_position, 1)
 
-bla.BresenhamsLineGenerationAlgorithm.bresenham(x1, y1, x2, y2, 3)
+    len_pixel_position_array = len(pixel_position_array)
 
-len_pixel_position_array = len(pixel_position_array)
+    calc_result_array_short = np.zeros((2, len(pixel_position_array)))
 
-calc_result_array_short = np.zeros((2, len(pixel_position_array)))
+    # element = pixel_position_array[0]
+    calc_result_array_index = 0
+    for element in pixel_position_array:
+        calc_result_array_short[1, calc_result_array_index] = element.vertical
+        calc_result_array_short[0, calc_result_array_index] = element.horizontal
+        calc_result_array_index += 1
 
-# element = pixel_position_array[0]
-calc_result_array_index = 0
-for element in pixel_position_array:
-    calc_result_array_short[1, calc_result_array_index] = element.vertical
-    calc_result_array_short[0, calc_result_array_index] = element.horizontal
-    calc_result_array_index += 1
+    # x = np.arange(0, 1, 0.05)
+    # y = np.power(x, 2)
 
-# x = np.arange(0, 1, 0.05)
-# y = np.power(x, 2)
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.set_xticks(np.arange(0, 30, 1))
+    ax.set_yticks(np.arange(0, 30, 1))
+    # plt.scatter(x, y)
+    plt.scatter(calc_result_array_short[0], calc_result_array_short[1])
+    point1 = [x1, y1]
+    point2 = [x2, y2]
+    x_values = [point1[0], point2[0]]  # gather x-values
+    y_values = [point1[1], point2[1]]  # gather y-values
+    plt.plot(x_values, y_values)
 
-fig = plt.figure()
-ax = fig.gca()
-ax.set_xticks(np.arange(0, 30, 1))
-ax.set_yticks(np.arange(0, 30, 1))
-# plt.scatter(x, y)
-plt.scatter(calc_result_array_short[0], calc_result_array_short[1])
-point1 = [x1, y1]
-point2 = [x2, y2]
-x_values = [point1[0], point2[0]]  # gather x-values
-y_values = [point1[1], point2[1]]  # gather y-values
-plt.plot(x_values, y_values)
+    # plt.grid()
+    plt.show()
 
-# plt.grid()
-plt.show()
+    # plot pixels
 
-# plot pixels
+    # define the size of the canvas
+    Row = 50
+    Cols = 50
+    # create an image
+    local_matrix_search = np.zeros((Row, Cols))
 
-# define the size of the canvas
-Row = 50
-Cols = 50
-# create an image
-local_matrix_search = np.zeros((Row, Cols))
+    # draw the pixels on the canvas
+    for element in pixel_position_array:
+        local_matrix_search[element.vertical][element.horizontal] = 1
+        calc_result_array_index += 1
 
-# draw the pixels on the canvas
-for element in pixel_position_array:
-    local_matrix_search[element.vertical][element.horizontal] = 1
-    calc_result_array_index += 1
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax1.imshow(local_matrix_search, interpolation='nearest', cmap=cm.Greys_r)
+    plt.show()
 
-fig = plt.figure()
-ax1 = fig.add_subplot(121)
-ax1.imshow(local_matrix_search, interpolation='nearest', cmap=cm.Greys_r)
-plt.show()
-
-# ax1.imshow(calc_result_array, interpolation='nearest', cmap=cm.Greys_r)
-# plt.show()
+    # ax1.imshow(calc_result_array, interpolation='nearest', cmap=cm.Greys_r)
+    # plt.show()
