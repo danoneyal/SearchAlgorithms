17aa938713ad2397403a851a04db363fb90a26ea
RF\edanon <danoneyal@gmail.com>
2022-01-13 09:04:43 +0200

    adding pixel posotion to unit test

--- archimedean_spiral.py
+++ archimedean_spiral.py
@@ -5,8 +5,8 @@ screen = turtle.Screen()
 offset_x = 0
 offset_y = 0
 factor = 1.0  # controls the distance between successive turnings
-a = 0  # turns the spiral
-smooth_factor = 20.0
+a = 4 # turns the spiral
+smooth_factor = 5.0
 for i in range(5000):
     angle = i / smooth_factor * math.pi
     x = offset_x + (angle * factor + a) * math.cos(angle)  # dx  theta * cos(theta)
--- test_bresenhams_line_generation_algorithm.py
+++ test_bresenhams_line_generation_algorithm.py
@@ -1,18 +1,28 @@
 import bresenhams_line_generation_algorithm as bl
-
+from bresenhams_line_generation_algorithm import PixelPosition
 import unittest
 
 
 # 12 test
 
 class TestUser(unittest.TestCase):
+    def setUp(self) -> None:
+        self.start_position = PixelPosition()
+        self.end_position = PixelPosition()
+
     # m slope is infinite
     def test_line_search__delta_x_zero__positive_y_direction(self):  #
         x1 = 45
         y1 = 45
         x2 = 45
         y2 = 1
-        bl.BresenhamsLineGenerationAlgorithm().bresenham(x1, y1, x2, y2, 2)
+        self.start_position.vertical = y1
+        self.start_position.horizontal = x1
+        self.end_position.vertical = y2
+        self.end_position.horizontal = x2
+
+        bl.BresenhamsLineGenerationAlgorithm().bresenham(start_position=self.start_position,
+                                                         end_position=self.end_position)
 
     def test_line_search__delta_x_zero__negative_y_direction(self):  #
         pass
@@ -25,27 +35,35 @@ class TestUser(unittest.TestCase):
         pass
 
     # delta_y_smaller_then_delta_x -1<m<1,abs(m)<1
-    def test_line_search__delta_y_smaller_then_delta_x__positive_x_positive_y_direction(self):# -1<m<1, abs(m)<1, x1<x2 , y1<y2
+    def test_line_search__delta_y_smaller_then_delta_x__positive_x_positive_y_direction(
+            self):  # -1<m<1, abs(m)<1, x1<x2 , y1<y2
         pass
 
-    def test_line_search__delta_y_smaller_then_delta_x__positive_x_negative_y_direction(self):# -1<m<1, abs(m)<1,x1<x2 , y1>y2
+    def test_line_search__delta_y_smaller_then_delta_x__positive_x_negative_y_direction(
+            self):  # -1<m<1, abs(m)<1,x1<x2 , y1>y2
         pass
 
-    def test_line_search__delta_y_smaller_then_delta_x__negative_x_positive_y_direction(self):#-1<m<1, abs(m)<1,x1>x2 , y1<y2
+    def test_line_search__delta_y_smaller_then_delta_x__negative_x_positive_y_direction(
+            self):  # -1<m<1, abs(m)<1,x1>x2 , y1<y2
         pass
 
-    def test_line_search__delta_y_smaller_then_delta_x__negative_x_negative_y_direction(self):#-1<m<1, abs(m)<1, x1>x2 , y1>y2
+    def test_line_search__delta_y_smaller_then_delta_x__negative_x_negative_y_direction(
+            self):  # -1<m<1, abs(m)<1, x1>x2 , y1>y2
         pass
 
     # delta_y_greater_then_delta_x -1<m<-1, abs(m)>1
-    def test_line_search__delta_y_greater_then_delta_x__positive_x_positive_y_direction(self):# -1<m<-1, abs(m)>1, x1<x2 , y1<y2
+    def test_line_search__delta_y_greater_then_delta_x__positive_x_positive_y_direction(
+            self):  # -1<m<-1, abs(m)>1, x1<x2 , y1<y2
         pass
 
-    def test_line_search__delta_y_greater_then_delta_x__positive_x_negative_y_direction(self):# -1<m<-1, abs(m)>1, x1<x2 , y1>y2
+    def test_line_search__delta_y_greater_then_delta_x__positive_x_negative_y_direction(
+            self):  # -1<m<-1, abs(m)>1, x1<x2 , y1>y2
         pass
 
-    def test_line_search__delta_y_greater_then_delta_x__negative_x_positive_y_direction(self):#-1<m<-1, abs(m)>1, x1>x2 , y1<y2
+    def test_line_search__delta_y_greater_then_delta_x__negative_x_positive_y_direction(
+            self):  # -1<m<-1, abs(m)>1, x1>x2 , y1<y2
         pass
 
-    def test_line_search__delta_y_greater_then_delta_x__negative_x_negative_y_direction(self):#-1<m<-1, abs(m)>1, x1>x2 , y1>y2
+    def test_line_search__delta_y_greater_then_delta_x__negative_x_negative_y_direction(
+            self):  # -1<m<-1, abs(m)>1, x1>x2 , y1>y2
         pass
